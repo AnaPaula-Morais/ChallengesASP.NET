623b3c869fdeb069585bd1d3601633adafc40aaa
Ana Paula <ap.almeidapm@gmail.com>
2026-08-12 22:14:47 -0300

    challenge 1 and create readme

---
+++ Challenge1/InterviewApi/InterviewApi/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using InterviewApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Pencil", Price = 1.99m },
+            new Product { Id = 2, Name = "Eraser", Price = 0.99m },
+            new Product { Id = 3, Name = "Notebook", Price = 3.99m }
+        };
+
+        [HttpGet(Name ="Products")]
+        public IActionResult GetProducts()
+        {
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
